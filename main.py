380bc651af5258d884c012eea2321c11216ed067
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2024-12-16 12:10:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 
-def is_int(input:str):
+def is_float(input:str):
     for i in input:
         if i in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]:
             continue
@@ -22,12 +22,38 @@ def parse_equation(input:str):
             raise Exception("More than 1 ´=´ in the equation !")
         elif i == "=" and not(equal):
             equal = True
-            continue
+            
         elif i in operators:
             if equal:
-                right.append(char)
+                if is_float(char):
+                    right.append(float(char))
+                else:
+                    right.append(char)
                 right.append(i)
+                
             else:
-                left.append(char)
+                if is_float(char):
+                    left.append(float(char))
+                else:
+                    left.append(char)
                 left.append(i)
+        else:
+            char += i
+        index += 1
+    
+    assert equal
+    
+    for i, j in enumerate(left):
+        if j == "x" and left[i+1] == "^":
+            left[i] = f"x^{i+2}"
+            del(left[i+1])
+            del(left[i+1])
             
+    for i, j in enumerate(right):
+        if j == "x" and right[i+1] == "^":
+            right[i] = f"x^{i+2}"
+            del(right[i+1])
+            del(right[i+1])
+            
+            
+    return (left, right)
