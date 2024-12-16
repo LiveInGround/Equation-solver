3a00da4aab1535e5c86c057d4d5e7e64bdb2a9f4
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2024-12-16 10:12:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import math
+
+def parse_equation(input:str):
+    left = []
+    right = []
+    equal = False
+    char = ""
+    index = 0
+    
+    operators = ("+", "-", "/", "X", "*")
+    
+    for i in input:
+        if i == "=" and equal:
+            raise Exception("More than 1 ´=´ in the equation !")
+        elif i == "=" and not(equal):
+            ...
