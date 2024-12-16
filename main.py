87f9d696beaf72dc7f6444dda26a93b8881ca8b7
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2024-12-16 11:40:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import math
 
 def is_int(input:str):
     for i in input:
-        if i in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
+        if i in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]:
+            continue
+        else:
+            return False
+    return True
 
 def parse_equation(input:str):
     left = []
@@ -17,4 +21,13 @@ def parse_equation(input:str):
         if i == "=" and equal:
             raise Exception("More than 1 ´=´ in the equation !")
         elif i == "=" and not(equal):
-            if
+            equal = True
+            continue
+        elif i in operators:
+            if equal:
+                right.append(char)
+                right.append(i)
+            else:
+                left.append(char)
+                left.append(i)
+            
