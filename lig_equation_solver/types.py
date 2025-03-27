7fad5563707c7835cfe598154e3234cd43598658
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-03-27 16:12:49 +0100

    Update types.py

--- lig_equation_solver/types.py
+++ lig_equation_solver/types.py
@@ -1,1 +1,27 @@
+import math
 
+class Square(object):
+    def __init__(self, value):
+        self.value=value
+        
+    def __repr__(self):
+        return f"sqrt({value})")m 
+        
+    def calculate(self):
+        return math.sqrt(self.value)
+        
+class Diff(object):
+    def __init__(self, up, down):
+        self.up = up
+        self.down = down
+        
+    def __repr__(self):
+        return f"DIFF({self.up};{self.down})"
+        
+    def calculate(self):
+        ... # calculate up part
+        ... # calculate down part
+        
+        ... # return up/down
+        
+        
